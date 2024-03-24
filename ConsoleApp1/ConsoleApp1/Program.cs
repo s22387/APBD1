64fcbf437eea187f3ee33ce3fcd294acc8ecfc7c
s22387 <s22387@pjwstk.edu.pl>
2024-03-24 22:13:32 +0100

    artificial function commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, dozo");
+double ObliczSrednia(int[] tablica)
+{
+    if (tablica == null || tablica.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta.");
+    }
+
+    int suma = 0;
+    foreach (int liczba in tablica)
+    {
+        suma += liczba;
+    }
+
+    double srednia = (double)suma / tablica.Length;
+    return srednia;
+}
